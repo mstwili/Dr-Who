9efd93f6127ef01584662e4b71f90446a3889215
Mei Yamasaki <112414904+mstwili@users.noreply.github.com>
2024-11-12 20:39:24 -0700

    Initial commit 2

---
+++ src/main/java/twili/dr_who/DrWhoDataGenerator.java
@@ -0,0 +1,11 @@
+package twili.dr_who;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DrWhoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
